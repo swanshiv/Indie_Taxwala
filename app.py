47c35164e09b72324e620ec1db983fd1b414a706
Shivranjan Kolvankar <shivranjan1689@gmail.com>
2025-07-17 09:54:59 +0530

    port update commit

--- app.py
+++ app.py
@@ -310,4 +310,6 @@ def advisor():
     return render_template('ask.html', advisor_message_html=advisor_message_html, session_id=session_id)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
